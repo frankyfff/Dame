398351750dcb80d322a5e2729ae5bf85094ccc12
Moe <schaunach@gmail.com>
2016-09-27 15:21:59 +0200

    44

--- Dame/src/gui/Brett.java
+++ Dame/src/gui/Brett.java
@@ -1,5 +1,9 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,6 +11,7 @@ import javax.swing.JPanel;
 public class Brett extends JFrame {
 
 	private JPanel jPanel1;
+	private Feld[][] feld = new Feld[10][10];
 	private JButton jButton;
 	
 	public Brett() {
@@ -20,8 +25,25 @@ public class Brett extends JFrame {
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		jPanel1.setLayout(new GridLayout(10,10));
+		jButton.setText("BBB");
+		jPanel1.add(jButton);
+		
+		getContentPane().add(jPanel1, BorderLayout.CENTER);
+		
+		setLocationRelativeTo(null);
+		
+		pack();
+		
 	}
 
 	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new Brett().setVisible(true);
+			}
+		});
 	}
 }
