e7edbcff9098cc3f4de967bee7b42ac62938a17c
Moe <schaunach@gmail.com>
2016-09-27 15:14:06 +0200

    33

--- Dame/src/gui/Brett.java
+++ Dame/src/gui/Brett.java
@@ -1,8 +1,27 @@
 package gui;
 
-public class Brett {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Brett extends JFrame {
+
+	private JPanel jPanel1;
+	private JButton jButton;
+	
+	public Brett() {
+		initComponents();
+	}
+	
+	private void initComponents() {
+		
+		jPanel1 = new JPanel();
+		jButton = new JButton();
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Test");
 	}
 }
---
+++ Dame/src/gui/Feld.java
@@ -0,0 +1,13 @@
+package gui;
+
+
+import javax.swing.JButton;
+
+public class Feld extends JButton  {
+
+	private Brett brett;
+	
+	public Feld(Brett brett) {
+		this.brett = brett;
+	}
+}
