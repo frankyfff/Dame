31f66b5172a3af556b66b992c12d08832163c63b
Moe <schaunach@gmail.com>
2016-09-27 14:53:13 +0200

    zzz

---
+++ TestApp/appClientModule/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Main xyz = new Main();
+		xyz.zaehler();
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+	
+	
+	
+	private void zaehler() {
+		
+
+		
+		for (int a = 0; a < 10; a++) {
+			System.out.println(a);
+		}
+		
+		
+	}
+
+}
