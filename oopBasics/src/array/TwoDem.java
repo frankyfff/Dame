09e908ccd98e4a8d5fd7065c2f6ccec4932979a2
Moe <schaunach@gmail.com>
2016-09-28 14:05:12 +0200

    2 Demensionale Arrays korrektur push

--- oopBasics/src/array/TwoDem.java
+++ oopBasics/src/array/TwoDem.java
@@ -1,5 +1,21 @@
 package array;
 
+import java.awt.Font;
+
 public class TwoDem {
 
+	int[][] myTestArray = new int[5][5];
+
+	public void init() {
+		for (int z = 0; z < myTestArray.length; z++) {
+			for (int sp = 0; sp < myTestArray[z].length; sp++) {
+				System.out.println("Array Index für z: " + z + " |--------| Array Index für sp: " + sp);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TwoDem dem = new TwoDem();
+		dem.init();
+	}
 }
